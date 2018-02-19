24f09c793eedc3df5db7d532734f28352959d230
robertfitstep <robertfitstep@gmail.com>
2018-02-19 19:45:04 +0100

    Add project files.

---
+++ Kalkulacka/Kalkulačka.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace Kalkulacka
+{
+    partial class Kalkulačka
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt1 = new System.Windows.Forms.Button();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.bt3 = new System.Windows.Forms.Button();
+            this.bt4 = new System.Windows.Forms.Button();
+            this.bt5 = new System.Windows.Forms.Button();
+            this.bt6 = new System.Windows.Forms.Button();
+            this.bt7 = new System.Windows.Forms.Button();
+            this.bt8 = new System.Windows.Forms.Button();
+            this.bt9 = new System.Windows.Forms.Button();
+            this.bt0 = new System.Windows.Forms.Button();
+            this.tbDisplej = new System.Windows.Forms.TextBox();
+            this.btRovnaSa = new System.Windows.Forms.Button();
+            this.btDeleno = new System.Windows.Forms.Button();
+            this.btKrat = new System.Windows.Forms.Button();
+            this.btMinus = new System.Windows.Forms.Button();
+            this.btPlus = new System.Windows.Forms.Button();
+            this.btZatvorkaKoniec = new System.Windows.Forms.Button();
+            this.btZatvorkaZaciatok = new System.Windows.Forms.Button();
+            this.btBackspace = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bt1
+            // 
+            this.bt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bt1.Location = new System.Drawing.Point(9, 110);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(82, 45);
+            this.bt1.TabIndex = 0;
+            this.bt1.Tag = "";
+            this.bt1.Text = "1";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.KlikNaTlacidlo);
+            // 
+            // bt2
+            // 
+            this.bt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bt2.Location = new System.Drawing.Point(97, 110);
+            this.bt2.Name = "bt2";
+            this.bt2.Size = new System.Drawing.Size(82, 45);
+            this.bt2.TabIndex = 0;
+            this.bt2.Tag = "";
+            this.bt2.Text = "2";
+            this.bt2.UseVisualStyleBackColor = true;
+            this.bt2.Click += new System.EventHandler(this.KlikNaTlacidlo);
+            // 
+            // bt3
+            // 
+            this.bt3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bt3.Location = new System.Drawing.Point(185, 110);
+            this.bt3.Name = "bt3";
+            this.bt3.Size = new System.Drawing.Size(82, 45);
+            this.bt3.TabIndex = 0;
+            this.bt3.Tag = "";
+            this.bt3.Text = "3";
+            this.bt3.UseVisualStyleBackColor = true;
+            this.bt3.Click += new System.EventHandler(this.KlikNaTlacidlo);
+            // 
+            // bt4
+            // 
+            this.bt4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bt4.Location = new System.Drawing.Point(9, 161);
+            this.bt4.Name = "bt4";
+            this.bt4.Size = new System.Drawing.Size(82, 45);
+            this.bt4.TabIndex = 0;
+            this.bt4.Tag = "";
+            this.bt4.Text = "4";
+            this.bt4.UseVisualStyleBackColor = true;
+            this.bt4.Click += new System.EventHandler(this.KlikNaTlacidlo);
+            // 
+            // bt5
+            // 
+            this.bt5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bt5.Location = new System.Drawing.Point(97, 161);
+            this.bt5.Name = "bt5";
+            this.bt5.Size = new System.Drawing.Size(82, 45);
+            this.bt5.TabIndex = 0;
+            this.bt5.Tag = "";
+            this.bt5.Text = "5";
+            this.bt5.UseVisualStyleBackColor = true;
+            this.bt5.Click += new System.EventHandler(this.KlikNaTlacidlo);
+            // 
+            // bt6
+            // 
+            this.bt6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bt6.Location = new System.Drawing.Point(185, 161);
+            this.bt6.Name = "bt6";
+            this.bt6.Size = new System.Drawing.Size(82, 45);
+            this.bt6.TabIndex = 0;
+            this.bt6.Tag = "";
+            this.bt6.Text = "6";
+            this.bt6.UseVisualStyleBackColor = true;
+            this.bt6.Click += new System.EventHandler(this.KlikNaTlacidlo);
+            // 
+            // bt7
+            // 
+            this.bt7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bt7.Location = new System.Drawing.Point(9, 212);
+            this.bt7.Name = "bt7";
+            this.bt7.Size = new System.Drawing.Size(82, 45);
+            this.bt7.TabIndex = 0;
+            this.bt7.Tag = "";
+            this.bt7.Text = "7";
+            this.bt7.UseVisualStyleBackColor = true;
+            this.bt7.Click += new System.EventHandler(this.KlikNaTlacidlo);
+            // 
+            // bt8
+            // 
+            this.bt8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bt8.Location = new System.Drawing.Point(97, 212);
+            this.bt8.Name = "bt8";
+            this.bt8.Size = new System.Drawing.Size(82, 45);
+            this.bt8.TabIndex = 0;
+            this.bt8.Tag = "";
+            this.bt8.Text = "8";
+            this.bt8.UseVisualStyleBackColor = true;
+            this.bt8.Click += new System.EventHandler(this.KlikNaTlacidlo);
+            // 
+            // bt9
+            // 
+            this.bt9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bt9.Location = new System.Drawing.Point(185, 212);
+            this.bt9.Name = "bt9";
+            this.bt9.Size = new System.Drawing.Size(82, 45);
+            this.bt9.TabIndex = 0;
+            this.bt9.Tag = "";
+            this.bt9.Text = "9";
+            this.bt9.UseVisualStyleBackColor = true;
+            this.bt9.Click += new System.EventHandler(this.KlikNaTlacidlo);
+            // 
+            // bt0
+            // 
+            this.bt0.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bt0.Location = new System.Drawing.Point(9, 263);
+            this.bt0.Name = "bt0";
+            this.bt0.Size = new System.Drawing.Size(258, 45);
+            this.bt0.TabIndex = 0;
+            this.bt0.Tag = "";
+            this.bt0.Text = "0";
+            this.bt0.UseVisualStyleBackColor = true;
+            this.bt0.Click += new System.EventHandler(this.KlikNaTlacidlo);
+            // 
+            // tbDisplej
+            // 
+            this.tbDisplej.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tbDisplej.Location = new System.Drawing.Point(12, 12);
+            this.tbDisplej.Name = "tbDisplej";
+            this.tbDisplej.Size = new System.Drawing.Size(343, 41);
+            this.tbDisplej.TabIndex = 1;
+            // 
+            // btRovnaSa
+            // 
+            this.btRovnaSa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btRovnaSa.Location = new System.Drawing.Point(273, 263);
+            this.btRovnaSa.Name = "btRovnaSa";
+            this.btRovnaSa.Size = new System.Drawing.Size(82, 45);
+            this.btRovnaSa.TabIndex = 0;
+            this.btRovnaSa.Text = "=";
+            this.btRovnaSa.UseVisualStyleBackColor = true;
+            this.btRovnaSa.Click += new System.EventHandler(this.KlikNaTlacidlo);
+            // 
+            // btDeleno
+            // 
+            this.btDeleno.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btDeleno.Location = new System.Drawing.Point(273, 212);
+            this.btDeleno.Name = "btDeleno";
+            this.btDeleno.Size = new System.Drawing.Size(82, 45);
+            this.btDeleno.TabIndex = 0;
+            this.btDeleno.Text = "/";
+            this.btDeleno.UseVisualStyleBackColor = true;
+            this.btDeleno.Click += new System.EventHandler(this.KlikNaTlacidlo);
+            // 
+            // btKrat
+            // 
+            this.btKrat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btKrat.Location = new System.Drawing.Point(273, 161);
+            this.btKrat.Name = "btKrat";
+            this.btKrat.Size = new System.Drawing.Size(82, 45);
+            this.btKrat.TabIndex = 0;
+            this.btKrat.Text = "*";
+            this.btKrat.UseVisualStyleBackColor = true;
+            this.btKrat.Click += new System.EventHandler(this.KlikNaTlacidlo);
+            // 
+            // btMinus
+            // 
+            this.btMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btMinus.Location = new System.Drawing.Point(273, 110);
+            this.btMinus.Name = "btMinus";
+            this.btMinus.Size = new System.Drawing.Size(82, 45);
+            this.btMinus.TabIndex = 0;
+            this.btMinus.Text = "-";
+            this.btMinus.UseVisualStyleBackColor = true;
+            this.btMinus.Click += new System.EventHandler(this.KlikNaTlacidlo);
+            // 
+            // btPlus
+            // 
+            this.btPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btPlus.Location = new System.Drawing.Point(273, 59);
+            this.btPlus.Name = "btPlus";
+            this.btPlus.Size = new System.Drawing.Size(82, 45);
+            this.btPlus.TabIndex = 0;
+            this.btPlus.Text = "+";
+            this.btPlus.UseVisualStyleBackColor = true;
+            this.btPlus.Click += new System.EventHandler(this.KlikNaTlacidlo);
+            // 
+            // btZatvorkaKoniec
+            // 
+            this.btZatvorkaKoniec.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btZatvorkaKoniec.Location = new System.Drawing.Point(185, 59);
+            this.btZatvorkaKoniec.Name = "btZatvorkaKoniec";
+            this.btZatvorkaKoniec.Size = new System.Drawing.Size(82, 45);
+            this.btZatvorkaKoniec.TabIndex = 0;
+            this.btZatvorkaKoniec.Text = ")";
+            this.btZatvorkaKoniec.UseVisualStyleBackColor = true;
+            this.btZatvorkaKoniec.Click += new System.EventHandler(this.KlikNaTlacidlo);
+            // 
+            // btZatvorkaZaciatok
+            // 
+            this.btZatvorkaZaciatok.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btZatvorkaZaciatok.Location = new System.Drawing.Point(97, 59);
+            this.btZatvorkaZaciatok.Name = "btZatvorkaZaciatok";
+            this.btZatvorkaZaciatok.Size = new System.Drawing.Size(82, 45);
+            this.btZatvorkaZaciatok.TabIndex = 0;
+            this.btZatvorkaZaciatok.Text = "(";
+            this.btZatvorkaZaciatok.UseVisualStyleBackColor = true;
+            this.btZatvorkaZaciatok.Click += new System.EventHandler(this.KlikNaTlacidlo);
+            // 
+            // btBackspace
+            // 
+            this.btBackspace.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btBackspace.Location = new System.Drawing.Point(9, 59);
+            this.btBackspace.Name = "btBackspace";
+            this.btBackspace.Size = new System.Drawing.Size(82, 45);
+            this.btBackspace.TabIndex = 0;
+            this.btBackspace.Text = "backspace <-";
+            this.btBackspace.UseVisualStyleBackColor = true;
+            this.btBackspace.Click += new System.EventHandler(this.KlikNaTlacidlo);
+            // 
+            // Kalkulačka
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 320);
+            this.Controls.Add(this.tbDisplej);
+            this.Controls.Add(this.bt0);
+            this.Controls.Add(this.bt9);
+            this.Controls.Add(this.bt8);
+            this.Controls.Add(this.bt7);
+            this.Controls.Add(this.bt6);
+            this.Controls.Add(this.bt5);
+            this.Controls.Add(this.bt4);
+            this.Controls.Add(this.btBackspace);
+            this.Controls.Add(this.btZatvorkaZaciatok);
+            this.Controls.Add(this.btZatvorkaKoniec);
+            this.Controls.Add(this.btPlus);
+            this.Controls.Add(this.btMinus);
+            this.Controls.Add(this.btKrat);
+            this.Controls.Add(this.btDeleno);
+            this.Controls.Add(this.btRovnaSa);
+            this.Controls.Add(this.bt3);
+            this.Controls.Add(this.bt2);
+            this.Controls.Add(this.bt1);
+            this.Name = "Kalkulačka";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.Button bt5;
+        private System.Windows.Forms.Button bt6;
+        private System.Windows.Forms.Button bt7;
+        private System.Windows.Forms.Button bt8;
+        private System.Windows.Forms.Button bt9;
+        private System.Windows.Forms.Button bt0;
+        private System.Windows.Forms.TextBox tbDisplej;
+        private System.Windows.Forms.Button btRovnaSa;
+        private System.Windows.Forms.Button btDeleno;
+        private System.Windows.Forms.Button btKrat;
+        private System.Windows.Forms.Button btMinus;
+        private System.Windows.Forms.Button btPlus;
+        private System.Windows.Forms.Button btZatvorkaKoniec;
+        private System.Windows.Forms.Button btZatvorkaZaciatok;
+        private System.Windows.Forms.Button btBackspace;
+    }
+}
+
---
+++ Kalkulacka/Kalkulačka.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Kalkulacka
+{
+    public partial class Kalkulačka : Form
+    {
+        public Kalkulačka()
+        {
+            InitializeComponent();
+
+            bt1.Tag = "1"; bt2.Tag = "2"; bt3.Tag = "3";
+            bt4.Tag = "4"; bt5.Tag = "5"; bt6.Tag = "6";
+            bt7.Tag = "7"; bt8.Tag = "8"; bt9.Tag = "9";
+                           bt0.Tag = "0";
+            
+            btPlus.Tag = "+";
+            btMinus.Tag = "-";
+            btKrat.Tag = "*";
+            btDeleno.Tag = "/";
+            btZatvorkaZaciatok.Tag = "(";
+            btZatvorkaKoniec.Tag = ")";
+            btRovnaSa.Tag = "=";
+            btBackspace.Tag = "Backspace";
+        }
+
+        private string textDispleja = "";
+        private DataTable dt = new DataTable();
+        
+        private void KlikNaTlacidlo(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            switch (b.Tag)
+            {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                case "(":
+                case ")":
+                    dopisNaDisplej(b.Tag.ToString());
+                    break;
+                case "=":
+                    vypocitaj();
+                    break;
+                case "Backspace":
+                    vymazPismeno();
+                    break;
+            }
+        }
+
+        private void vymazPismeno()
+        {
+            textDispleja = textDispleja.Substring(0,textDispleja.Length-1);
+            tbDisplej.Text = textDispleja;
+        }
+
+        private void dopisNaDisplej(string znak)
+        {
+            textDispleja += znak;
+            tbDisplej.Text = textDispleja;
+        }
+
+        private void vypocitaj()
+        {
+            try
+            {
+                textDispleja = dt.Compute(textDispleja, "").ToString();
+                tbDisplej.Text = textDispleja;
+                textDispleja = "";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Hlaska: " + e.Message);
+                zobrazChybu();
+            }
+        }
+
+        private void zobrazChybu()
+        {
+            tbDisplej.Text = "err";
+            zapnutTlacidla(false);
+            Thread.Sleep(3000);
+            tbDisplej.Text = textDispleja;
+            zapnutTlacidla(true);
+        }
+
+        private void zapnutTlacidla(bool prepinac)
+        {
+            Console.WriteLine("zapnut tlacidla = {0}", prepinac);
+            foreach (var control in this.Controls)
+            {
+                if (control.GetType() == typeof(Button))
+                {
+                    Button b = (Button)control;
+                    b.Enabled = prepinac;
+                }
+            }
+        }
+
+    }
+}
